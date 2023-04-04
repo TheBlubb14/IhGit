fc2e22735bcca8ca465b1867ccc00cae1e0c0a55
TheBlubb14 <TheBlubb14@users.noreply.github.com>
2023-04-04 22:31:51 +0200

    Add project files.

---
+++ IhGit/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace IhGit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            buttonClear = new Button();
+            textBoxFeatureName = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBoxCommits = new TextBox();
+            buttonUpmerge = new Button();
+            textBoxDescription = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxTitle = new TextBox();
+            textBoxOutput = new TextBox();
+            buttonPullRequest = new Button();
+            buttonFetch = new Button();
+            buttonPush = new Button();
+            checkBoxDryRun = new CheckBox();
+            buttonNewBranch = new Button();
+            buttonPullRequestMultiple = new Button();
+            checkBoxStartOnSameVersion = new CheckBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(634, 483);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Status";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(1061, 483);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(75, 23);
+            buttonClear.TabIndex = 2;
+            buttonClear.Text = "Clear";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // textBoxFeatureName
+            // 
+            textBoxFeatureName.Location = new Point(131, 7);
+            textBoxFeatureName.Name = "textBoxFeatureName";
+            textBoxFeatureName.Size = new Size(578, 23);
+            textBoxFeatureName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Feature Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Commits";
+            // 
+            // textBoxCommits
+            // 
+            textBoxCommits.Location = new Point(131, 36);
+            textBoxCommits.Multiline = true;
+            textBoxCommits.Name = "textBoxCommits";
+            textBoxCommits.Size = new Size(578, 203);
+            textBoxCommits.TabIndex = 5;
+            // 
+            // buttonUpmerge
+            // 
+            buttonUpmerge.Location = new Point(131, 483);
+            buttonUpmerge.Name = "buttonUpmerge";
+            buttonUpmerge.Size = new Size(75, 23);
+            buttonUpmerge.TabIndex = 7;
+            buttonUpmerge.Text = "Upmerge";
+            buttonUpmerge.UseVisualStyleBackColor = true;
+            buttonUpmerge.Click += buttonUpmerge_Click;
+            // 
+            // textBoxDescription
+            // 
+            textBoxDescription.Location = new Point(131, 274);
+            textBoxDescription.Multiline = true;
+            textBoxDescription.Name = "textBoxDescription";
+            textBoxDescription.Size = new Size(578, 203);
+            textBoxDescription.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 277);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Description";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 253);
+            label4.Name = "label4";
+            label4.Size = new Size(29, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Title";
+            // 
+            // textBoxTitle
+            // 
+            textBoxTitle.Location = new Point(131, 245);
+            textBoxTitle.Name = "textBoxTitle";
+            textBoxTitle.Size = new Size(578, 23);
+            textBoxTitle.TabIndex = 10;
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.Location = new Point(715, 7);
+            textBoxOutput.Multiline = true;
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(421, 470);
+            textBoxOutput.TabIndex = 12;
+            // 
+            // buttonPullRequest
+            // 
+            buttonPullRequest.Location = new Point(516, 483);
+            buttonPullRequest.Name = "buttonPullRequest";
+            buttonPullRequest.Size = new Size(112, 23);
+            buttonPullRequest.TabIndex = 13;
+            buttonPullRequest.Text = "Pull Request";
+            buttonPullRequest.UseVisualStyleBackColor = true;
+            buttonPullRequest.Click += buttonPullRequest_Click;
+            // 
+            // buttonFetch
+            // 
+            buttonFetch.Location = new Point(212, 483);
+            buttonFetch.Name = "buttonFetch";
+            buttonFetch.Size = new Size(75, 23);
+            buttonFetch.TabIndex = 14;
+            buttonFetch.Text = "Fetch";
+            buttonFetch.UseVisualStyleBackColor = true;
+            buttonFetch.Click += buttonFetch_Click;
+            // 
+            // buttonPush
+            // 
+            buttonPush.Location = new Point(293, 483);
+            buttonPush.Name = "buttonPush";
+            buttonPush.Size = new Size(75, 23);
+            buttonPush.TabIndex = 15;
+            buttonPush.Text = "Push";
+            buttonPush.UseVisualStyleBackColor = true;
+            buttonPush.Click += buttonPush_Click;
+            // 
+            // checkBoxDryRun
+            // 
+            checkBoxDryRun.AutoSize = true;
+            checkBoxDryRun.Location = new Point(374, 487);
+            checkBoxDryRun.Name = "checkBoxDryRun";
+            checkBoxDryRun.Size = new Size(68, 19);
+            checkBoxDryRun.TabIndex = 16;
+            checkBoxDryRun.Text = "Dry Run";
+            checkBoxDryRun.UseVisualStyleBackColor = true;
+            // 
+            // buttonNewBranch
+            // 
+            buttonNewBranch.Location = new Point(131, 512);
+            buttonNewBranch.Name = "buttonNewBranch";
+            buttonNewBranch.Size = new Size(111, 23);
+            buttonNewBranch.TabIndex = 17;
+            buttonNewBranch.Text = "New Branch";
+            buttonNewBranch.UseVisualStyleBackColor = true;
+            buttonNewBranch.Click += buttonNewBranch_Click;
+            // 
+            // buttonPullRequestMultiple
+            // 
+            buttonPullRequestMultiple.Location = new Point(516, 512);
+            buttonPullRequestMultiple.Name = "buttonPullRequestMultiple";
+            buttonPullRequestMultiple.Size = new Size(193, 23);
+            buttonPullRequestMultiple.TabIndex = 18;
+            buttonPullRequestMultiple.Text = "Pull Request Multiple";
+            buttonPullRequestMultiple.UseVisualStyleBackColor = true;
+            buttonPullRequestMultiple.Click += buttonPullRequestMultiple_Click;
+            // 
+            // checkBoxStartOnSameVersion
+            // 
+            checkBoxStartOnSameVersion.AutoSize = true;
+            checkBoxStartOnSameVersion.Location = new Point(516, 541);
+            checkBoxStartOnSameVersion.Name = "checkBoxStartOnSameVersion";
+            checkBoxStartOnSameVersion.Size = new Size(139, 19);
+            checkBoxStartOnSameVersion.TabIndex = 19;
+            checkBoxStartOnSameVersion.Text = "Start on same version";
+            checkBoxStartOnSameVersion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1148, 722);
+            Controls.Add(checkBoxStartOnSameVersion);
+            Controls.Add(buttonPullRequestMultiple);
+            Controls.Add(buttonNewBranch);
+            Controls.Add(checkBoxDryRun);
+            Controls.Add(buttonPush);
+            Controls.Add(buttonFetch);
+            Controls.Add(buttonPullRequest);
+            Controls.Add(textBoxOutput);
+            Controls.Add(label4);
+            Controls.Add(textBoxTitle);
+            Controls.Add(label3);
+            Controls.Add(textBoxDescription);
+            Controls.Add(buttonUpmerge);
+            Controls.Add(label2);
+            Controls.Add(textBoxCommits);
+            Controls.Add(label1);
+            Controls.Add(textBoxFeatureName);
+            Controls.Add(buttonClear);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button buttonClear;
+        private TextBox textBoxFeatureName;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxCommits;
+        private Button buttonUpmerge;
+        private TextBox textBoxDescription;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxTitle;
+        private TextBox textBoxOutput;
+        private Button buttonPullRequest;
+        private Button buttonFetch;
+        private Button buttonPush;
+        private CheckBox checkBoxDryRun;
+        private Button buttonNewBranch;
+        private Button buttonPullRequestMultiple;
+        private CheckBox checkBoxStartOnSameVersion;
+    }
+}
---
+++ IhGit/Form1.cs
@@ -0,0 +1,375 @@
+using AdysTech.CredentialManager;
+using CliWrap;
+using CliWrap.Buffered;
+using LibGit2Sharp;
+using LibGit2Sharp.Handlers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace IhGit
+{
+    public partial class Form1 : Form
+    {
+        const int max_support_version = 17;
+        private string userName = "TheBlubb14";
+        private string baseBranch = "stable";
+        private string newBranch = "work/TestBlubb";
+        private string repoPath = "C:\\Dev\\Projects\\GitHub\\paxcontrol";
+
+        // https://github.com/libgit2/libgit2sharp
+        //private Repository repo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Git("status");
+        }
+
+        private async Task Git(params string[] arg)
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log("Dry run: git " + string.Join(" ", arg));
+                return;
+            }
+
+            var cmd = Cli.Wrap("git.exe")
+                .WithArguments(arg)
+                .WithWorkingDirectory(repoPath) | Log;
+            await cmd.ExecuteAsync();
+
+        }
+
+        private void Log(string m)
+        {
+            textBoxOutput.Invoke(() => textBoxOutput.Text += m + Environment.NewLine);
+        }
+
+
+        private string? Fetch()
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log("Dry run: git fetch origin");
+                return null;
+            }
+
+            using var repo = new Repository(repoPath);
+
+            var log = "";
+            var remote = repo.Network.Remotes["origin"];
+            var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+
+            FetchOptions options = new()
+            {
+                CredentialsProvider = GetCredentialsHandler()
+            };
+
+            Commands.Fetch(repo, remote.Name, refSpecs, options, log);
+            return string.IsNullOrWhiteSpace(log) ? null : log;
+        }
+
+        private CredentialsHandler GetCredentialsHandler()
+        {
+            var cred = CredentialManager.GetCredentials("git:https://github.com");
+            return new CredentialsHandler((url, usernameFromUrl, types) =>
+            new UsernamePasswordCredentials()
+            {
+                Username = userName,
+                Password = cred.Password,
+            });
+        }
+
+        private void CreateNewBranch(string name)
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log($"Dry run: git checkout -b {name}");
+                return;
+            }
+
+            using var repo = new Repository(repoPath);
+            var branch = repo.CreateBranch(name);
+
+            Commands.Checkout(repo, branch);
+        }
+
+        private void Push()
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log($"Dry run: git push -u origin");
+                return;
+            }
+
+            using var repo = new Repository(repoPath);
+            PushOptions options = new()
+            {
+                CredentialsProvider = GetCredentialsHandler(),
+            };
+            repo.Network.Push(repo.Network.Remotes["origin"], repo.Head.CanonicalName, options);
+        }
+
+        // Select base branch                               support/v4.16
+        // Input Branch name, without base branch           FastId
+        // Input ordered list of commits to cherry pick     xxxx, yyyy
+        // -- loop
+        // Fetch next support branch up                     support/v4.17
+        // Make new branch from up and checkout             patch/v4.17/FastId
+        // Cherry pick commits                              xxxx, yyyy
+        // Open PR Website                                  https://github.com/airsphere-gmbh/PaxControl/compare/support/v4.17...patch/v4.17/FastIdPatch?quick_pull=1&labels=upmerge
+        // -- loop
+
+        protected override void OnClosed(EventArgs e)
+        {
+            //repo.Dispose();
+            base.OnClosed(e);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Clear();
+        }
+
+        // Only when we have already once a pr made, then we need to be in the next support branch
+        private async void buttonUpmerge_Click(object sender, EventArgs e)
+        {
+            var commits = textBoxCommits.Text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Fetch();
+            await UpmergeOne(commits);
+        }
+
+        private async Task UpmergeOne(string[] commits)
+        {
+            var info = GetBranchInfo(checkBoxStartOnSameVersion.Checked);
+
+            if (info is null)
+                return;
+
+            if (!SwitchBranch(info.NewOrigin))
+                return;
+
+            CreateNewBranch(info.New);
+
+            foreach (var commit in commits)
+            {
+                await Git("cherry-pick", commit);
+            }
+            Push();
+            PullRequest();
+        }
+
+        private void PullRequest()
+        {
+            var info = GetBranchInfo(true);
+
+            if (info is null)
+                return;
+
+            var title = "";
+            var description = "";
+
+            if (!string.IsNullOrWhiteSpace(textBoxTitle.Text))
+            {
+                title = "&title=" + HttpUtility.UrlEncode(textBoxTitle.Text);
+            }
+
+            if (!string.IsNullOrWhiteSpace(textBoxDescription.Text))
+            {
+                description = "&body=" + HttpUtility.UrlEncode(textBoxDescription.Text);
+            }
+
+            var branchName = info.Origin;
+            var newBranchName = info.Current;
+            OpenUrl($"https://github.com/airsphere-gmbh/PaxControl/compare/{branchName}...{newBranchName}?quick_pull=1&labels=upmerge{title}{description}");
+        }
+
+        private bool SwitchBranch(string newBranch)
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log($"Dry run: git switch {newBranch}");
+                return true;
+            }
+
+            using var repo = new Repository(repoPath);
+            var branch = repo.Branches[newBranch];
+
+            if (branch is null)
+                return false;
+
+            var currentBranch = Commands.Checkout(repo, branch);
+            return currentBranch is not null;
+        }
+
+        private void OpenUrl(string url)
+        {
+            if (checkBoxDryRun.Checked)
+            {
+                Log($"Dry run: open url {url}");
+                return;
+            }
+
+            Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true
+            });
+        }
+
+        private string? NewBranch()
+        {
+            var info = GetBranchInfo(checkBoxStartOnSameVersion.Checked);
+
+            if (info is null)
+                return null;
+
+            //using var repo = new Repository(repoPath);
+            //var branchName = repo.Head.FriendlyName; // support/v4.17
+            //var splitBranch = branchName.Split('/');
+
+            //var isSupport = splitBranch[0] == "support";
+            //var isStable = splitBranch[0] == "stable";
+            //if (!isStable && !isSupport && !checkBoxDryRun.Checked)
+            //    return null;
+
+            //var featureName = textBoxFeatureName.Text?.Trim();
+            //if (string.IsNullOrWhiteSpace(featureName))
+            //    return null;
+
+            //string? newBranchName;
+            //if (isSupport)
+            //{
+            //    var version = splitBranch[1];
+            //    newBranchName = $"patch/{version}/{featureName}";
+            //}
+            //else
+            //{
+            //    newBranchName = $"work/{featureName}";
+            //}
+
+            //var newBranchName = $"{info.New}/{featureName}";
+            CreateNewBranch(info.New);
+            return info.New;
+        }
+
+        private string CurrentBranchName()
+        {
+            using var repo = new Repository(repoPath);
+            var branchName = repo.Head.FriendlyName; // support/v4.17
+            return branchName;
+        }
+
+        private void buttonFetch_Click(object sender, EventArgs e)
+        {
+            Fetch();
+        }
+
+        private void buttonPush_Click(object sender, EventArgs e)
+        {
+            Push();
+        }
+
+        private void buttonNewBranch_Click(object sender, EventArgs e)
+        {
+            NewBranch();
+        }
+
+        private async void buttonPullRequestMultiple_Click(object sender, EventArgs e)
+        {
+            var commits = textBoxCommits.Text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            // support/v4.16
+            BranchInfo? info = null;
+            do
+            {
+                Fetch();
+                info = GetBranchInfo(checkBoxStartOnSameVersion.Checked);
+
+                if (info is null)
+                    break;
+
+                //if (!SwitchBranch(info.NewOrigin))
+                //    break;
+
+                await UpmergeOne(commits);
+
+                if (checkBoxDryRun.Checked)
+                    Log("Warning, will not walk through each upmerge in dry-run");
+
+            } while (!info.IsStable && !checkBoxDryRun.Checked);
+        }
+
+        record BranchInfo(string Current, string New, bool IsStable, string Origin, string NewOrigin);
+        private BranchInfo? GetBranchInfo(bool doNotCountVersionUp)
+        {
+            var currentBranch = CurrentBranchName();
+            var currentBranchSplitted = currentBranch.Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+            var isStable = false;
+            var version = 0;
+
+            if (currentBranchSplitted.Length < 1)
+            {
+                if (currentBranchSplitted[0] == "stable")
+                {
+                    isStable = true;
+                }
+                else
+                {
+                    Log("Could not parse branch version");
+                    return null;
+                }
+            }
+            else if (currentBranchSplitted[0] == "work")
+            {
+                isStable = true;
+            }
+            else
+            {
+                var supportMatch = Regex.Match(currentBranchSplitted[1], "v4.(1\\d)");
+                if (supportMatch.Success)
+                {
+                    version = int.Parse(supportMatch.Groups[1].Value) + 1;
+
+                    if (doNotCountVersionUp)
+                        version--;
+                }
+                else
+                {
+                    Log($"Could not parse branch version: {currentBranchSplitted[1]}");
+                    return null;
+                }
+            }
+
+            var shouldBaseOnStable = isStable || version > max_support_version;
+            var originVersion = version - (doNotCountVersionUp ? 0 : 1);
+
+            var featureName = textBoxFeatureName.Text?.Trim();
+            if (string.IsNullOrWhiteSpace(featureName))
+                return null;
+
+            var newBranchName = shouldBaseOnStable ? "work" : $"patch/v4.{version}";
+            newBranchName += $"/{featureName}";
+
+            return new BranchInfo(currentBranch,
+                newBranchName,
+                isStable,
+                shouldBaseOnStable ? "stable" : $"support/v4.{originVersion}",
+                shouldBaseOnStable ? "stable" : $"support/v4.{version}");
+        }
+
+        private void buttonPullRequest_Click(object sender, EventArgs e)
+        {
+            PullRequest();
+        }
+    }
+}
