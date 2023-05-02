4a6a9065e5373aa7b6d3143c4f9bf268395a2006
TheBlubb14 <TheBlubb14@users.noreply.github.com>
2023-05-02 11:36:29 +0200

    Better UI

--- IhGit/Form1.Designer.cs
+++ IhGit/Form1.Designer.cs
@@ -57,228 +57,248 @@
             // 
             // button1
             // 
-            button1.Location = new Point(634, 483);
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Location = new Point(343, 295);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
+            button1.Size = new Size(112, 23);
+            button1.TabIndex = 14;
             button1.Text = "Status";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // buttonClear
             // 
-            buttonClear.Location = new Point(1061, 483);
+            buttonClear.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonClear.Location = new Point(478, 440);
             buttonClear.Name = "buttonClear";
-            buttonClear.Size = new Size(75, 23);
-            buttonClear.TabIndex = 2;
-            buttonClear.Text = "Clear";
+            buttonClear.Size = new Size(193, 23);
+            buttonClear.TabIndex = 23;
+            buttonClear.Text = "Clear Logs";
             buttonClear.UseVisualStyleBackColor = true;
             buttonClear.Click += buttonClear_Click;
             // 
             // textBoxFeatureName
             // 
-            textBoxFeatureName.Location = new Point(131, 7);
+            textBoxFeatureName.Location = new Point(13, 27);
             textBoxFeatureName.Name = "textBoxFeatureName";
-            textBoxFeatureName.Size = new Size(578, 23);
-            textBoxFeatureName.TabIndex = 3;
+            textBoxFeatureName.Size = new Size(323, 23);
+            textBoxFeatureName.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 15);
+            label1.Location = new Point(13, 9);
             label1.Name = "label1";
             label1.Size = new Size(81, 15);
-            label1.TabIndex = 4;
+            label1.TabIndex = 0;
             label1.Text = "Feature Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 39);
+            label2.Location = new Point(13, 53);
             label2.Name = "label2";
             label2.Size = new Size(56, 15);
-            label2.TabIndex = 6;
+            label2.TabIndex = 2;
             label2.Text = "Commits";
             // 
             // textBoxCommits
             // 
-            textBoxCommits.Location = new Point(131, 36);
+            textBoxCommits.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBoxCommits.Location = new Point(13, 71);
             textBoxCommits.Multiline = true;
             textBoxCommits.Name = "textBoxCommits";
-            textBoxCommits.Size = new Size(578, 203);
-            textBoxCommits.TabIndex = 5;
+            textBoxCommits.Size = new Size(323, 203);
+            textBoxCommits.TabIndex = 3;
             // 
             // buttonUpmerge
             // 
-            buttonUpmerge.Location = new Point(131, 483);
+            buttonUpmerge.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonUpmerge.Location = new Point(478, 382);
             buttonUpmerge.Name = "buttonUpmerge";
             buttonUpmerge.Size = new Size(75, 23);
-            buttonUpmerge.TabIndex = 7;
+            buttonUpmerge.TabIndex = 21;
             buttonUpmerge.Text = "Upmerge";
             buttonUpmerge.UseVisualStyleBackColor = true;
             buttonUpmerge.Click += buttonUpmerge_Click;
             // 
             // textBoxDescription
             // 
-            textBoxDescription.Location = new Point(131, 274);
+            textBoxDescription.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBoxDescription.Location = new Point(343, 71);
             textBoxDescription.Multiline = true;
             textBoxDescription.Name = "textBoxDescription";
-            textBoxDescription.Size = new Size(578, 203);
-            textBoxDescription.TabIndex = 8;
+            textBoxDescription.Size = new Size(323, 203);
+            textBoxDescription.TabIndex = 7;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 277);
+            label3.Location = new Point(343, 53);
             label3.Name = "label3";
             label3.Size = new Size(67, 15);
-            label3.TabIndex = 9;
+            label3.TabIndex = 6;
             label3.Text = "Description";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 253);
+            label4.Location = new Point(343, 9);
             label4.Name = "label4";
             label4.Size = new Size(29, 15);
-            label4.TabIndex = 11;
+            label4.TabIndex = 4;
             label4.Text = "Title";
             // 
             // textBoxTitle
             // 
-            textBoxTitle.Location = new Point(131, 245);
+            textBoxTitle.Location = new Point(343, 27);
             textBoxTitle.Name = "textBoxTitle";
-            textBoxTitle.Size = new Size(578, 23);
-            textBoxTitle.TabIndex = 10;
+            textBoxTitle.Size = new Size(323, 23);
+            textBoxTitle.TabIndex = 5;
             // 
             // textBoxOutput
             // 
-            textBoxOutput.Location = new Point(715, 7);
+            textBoxOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxOutput.Location = new Point(682, 0);
             textBoxOutput.Multiline = true;
             textBoxOutput.Name = "textBoxOutput";
-            textBoxOutput.Size = new Size(421, 470);
-            textBoxOutput.TabIndex = 12;
+            textBoxOutput.ScrollBars = ScrollBars.Both;
+            textBoxOutput.Size = new Size(458, 474);
+            textBoxOutput.TabIndex = 24;
             // 
             // buttonPullRequest
             // 
-            buttonPullRequest.Location = new Point(516, 483);
+            buttonPullRequest.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonPullRequest.Location = new Point(343, 411);
             buttonPullRequest.Name = "buttonPullRequest";
             buttonPullRequest.Size = new Size(112, 23);
-            buttonPullRequest.TabIndex = 13;
+            buttonPullRequest.TabIndex = 18;
             buttonPullRequest.Text = "Pull Request";
             buttonPullRequest.UseVisualStyleBackColor = true;
             buttonPullRequest.Click += buttonPullRequest_Click;
             // 
             // buttonFetch
             // 
-            buttonFetch.Location = new Point(212, 483);
+            buttonFetch.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonFetch.Location = new Point(343, 324);
             buttonFetch.Name = "buttonFetch";
-            buttonFetch.Size = new Size(75, 23);
-            buttonFetch.TabIndex = 14;
+            buttonFetch.Size = new Size(112, 23);
+            buttonFetch.TabIndex = 15;
             buttonFetch.Text = "Fetch";
             buttonFetch.UseVisualStyleBackColor = true;
             buttonFetch.Click += buttonFetch_Click;
             // 
             // buttonPush
             // 
-            buttonPush.Location = new Point(293, 483);
+            buttonPush.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonPush.Location = new Point(343, 382);
             buttonPush.Name = "buttonPush";
-            buttonPush.Size = new Size(75, 23);
-            buttonPush.TabIndex = 15;
+            buttonPush.Size = new Size(112, 23);
+            buttonPush.TabIndex = 17;
             buttonPush.Text = "Push";
             buttonPush.UseVisualStyleBackColor = true;
             buttonPush.Click += buttonPush_Click;
             // 
             // checkBoxDryRun
             // 
+            checkBoxDryRun.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             checkBoxDryRun.AutoSize = true;
-            checkBoxDryRun.Location = new Point(374, 487);
+            checkBoxDryRun.Location = new Point(478, 299);
             checkBoxDryRun.Name = "checkBoxDryRun";
             checkBoxDryRun.Size = new Size(68, 19);
-            checkBoxDryRun.TabIndex = 16;
+            checkBoxDryRun.TabIndex = 19;
             checkBoxDryRun.Text = "Dry Run";
             checkBoxDryRun.UseVisualStyleBackColor = true;
             // 
             // buttonNewBranch
             // 
-            buttonNewBranch.Location = new Point(131, 512);
+            buttonNewBranch.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonNewBranch.Location = new Point(343, 353);
             buttonNewBranch.Name = "buttonNewBranch";
-            buttonNewBranch.Size = new Size(111, 23);
-            buttonNewBranch.TabIndex = 17;
+            buttonNewBranch.Size = new Size(112, 23);
+            buttonNewBranch.TabIndex = 16;
             buttonNewBranch.Text = "New Branch";
             buttonNewBranch.UseVisualStyleBackColor = true;
             buttonNewBranch.Click += buttonNewBranch_Click;
             // 
             // buttonPullRequestMultiple
             // 
-            buttonPullRequestMultiple.Location = new Point(516, 512);
+            buttonPullRequestMultiple.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonPullRequestMultiple.Location = new Point(478, 411);
             buttonPullRequestMultiple.Name = "buttonPullRequestMultiple";
             buttonPullRequestMultiple.Size = new Size(193, 23);
-            buttonPullRequestMultiple.TabIndex = 18;
-            buttonPullRequestMultiple.Text = "Pull Request Multiple";
+            buttonPullRequestMultiple.TabIndex = 22;
+            buttonPullRequestMultiple.Text = "Upmerge all";
             buttonPullRequestMultiple.UseVisualStyleBackColor = true;
             buttonPullRequestMultiple.Click += buttonPullRequestMultiple_Click;
             // 
             // checkBoxStartOnSameVersion
             // 
+            checkBoxStartOnSameVersion.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             checkBoxStartOnSameVersion.AutoSize = true;
-            checkBoxStartOnSameVersion.Location = new Point(516, 541);
+            checkBoxStartOnSameVersion.Location = new Point(478, 328);
             checkBoxStartOnSameVersion.Name = "checkBoxStartOnSameVersion";
             checkBoxStartOnSameVersion.Size = new Size(139, 19);
-            checkBoxStartOnSameVersion.TabIndex = 19;
+            checkBoxStartOnSameVersion.TabIndex = 20;
             checkBoxStartOnSameVersion.Text = "Start on same version";
             checkBoxStartOnSameVersion.UseVisualStyleBackColor = true;
             // 
             // textBoxUserName
             // 
-            textBoxUserName.Location = new Point(131, 570);
+            textBoxUserName.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            textBoxUserName.Location = new Point(13, 295);
             textBoxUserName.Name = "textBoxUserName";
-            textBoxUserName.Size = new Size(311, 23);
-            textBoxUserName.TabIndex = 20;
+            textBoxUserName.Size = new Size(323, 23);
+            textBoxUserName.TabIndex = 9;
             textBoxUserName.Text = "TheBlubb14";
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label5.AutoSize = true;
-            label5.Location = new Point(12, 578);
+            label5.Location = new Point(13, 277);
             label5.Name = "label5";
             label5.Size = new Size(60, 15);
-            label5.TabIndex = 21;
+            label5.TabIndex = 8;
             label5.Text = "Username";
             // 
             // label6
             // 
+            label6.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label6.AutoSize = true;
-            label6.Location = new Point(12, 607);
+            label6.Location = new Point(13, 321);
             label6.Name = "label6";
             label6.Size = new Size(34, 15);
-            label6.TabIndex = 23;
+            label6.TabIndex = 10;
             label6.Text = "Repo";
             // 
             // textBoxRepo
             // 
-            textBoxRepo.Location = new Point(131, 599);
+            textBoxRepo.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            textBoxRepo.Location = new Point(13, 339);
             textBoxRepo.Name = "textBoxRepo";
-            textBoxRepo.Size = new Size(311, 23);
-            textBoxRepo.TabIndex = 22;
+            textBoxRepo.Size = new Size(323, 23);
+            textBoxRepo.TabIndex = 11;
             textBoxRepo.Text = "C:\\Dev\\Projects\\GitHub\\paxcontrol";
             // 
             // buttonDaniel
             // 
-            buttonDaniel.Location = new Point(131, 628);
+            buttonDaniel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonDaniel.Location = new Point(12, 368);
             buttonDaniel.Name = "buttonDaniel";
             buttonDaniel.Size = new Size(75, 23);
-            buttonDaniel.TabIndex = 24;
+            buttonDaniel.TabIndex = 12;
             buttonDaniel.Text = "Daniel";
             buttonDaniel.UseVisualStyleBackColor = true;
             buttonDaniel.Click += buttonDaniel_Click;
             // 
             // buttonSpike
             // 
-            buttonSpike.Location = new Point(212, 628);
+            buttonSpike.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonSpike.Location = new Point(93, 368);
             buttonSpike.Name = "buttonSpike";
             buttonSpike.Size = new Size(75, 23);
-            buttonSpike.TabIndex = 25;
+            buttonSpike.TabIndex = 13;
             buttonSpike.Text = "Spike";
             buttonSpike.UseVisualStyleBackColor = true;
             buttonSpike.Click += buttonSpike_Click;
@@ -287,7 +307,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1148, 722);
+            ClientSize = new Size(1140, 474);
             Controls.Add(buttonSpike);
             Controls.Add(buttonDaniel);
             Controls.Add(label6);
@@ -314,7 +334,7 @@
             Controls.Add(buttonClear);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "IhGit";
             ResumeLayout(false);
             PerformLayout();
         }
