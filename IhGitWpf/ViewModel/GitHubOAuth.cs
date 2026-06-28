17e41d99ab0450376f75919ce2776341de952810
Daniel Henk <TheBlubb14@users.noreply.github.com>
2026-06-28 23:48:49 +0200

    Change to GitHub Device Flow
    Changes the authentication from an GitHub API token to the device flow pattern.
    
    Add TPM based encryption of GitHub token

---
+++ IhGitWpf/ViewModel/GitHubOAuth.cs
@@ -0,0 +1,15 @@
+﻿using Octokit;
+
+namespace IhGitWpf.ViewModel;
+
+public record GitHubOAuth(string? AccessToken)
+{
+    public GitHubOAuth() : this(AccessToken: null)
+    {
+
+    }
+
+    public GitHubOAuth(OauthToken oauthToken) : this(oauthToken.AccessToken)
+    {
+    }
+}
